58f7525e03178a2cea98b78a32588be8f0da7fa2
aaruc10 <145377891+aaruc10@users.noreply.github.com>
2024-08-07 10:42:19 +0530

    program9.c

--- program9.c
+++ program9.c
@@ -22,7 +22,7 @@
 //  Description :       Used to check whether number is divisible by 3 & 5
 //  Input :             Integer
 //  Output :            Boolean
-//  Author :            Piyush Manohar Khairnar
+//  Author :            Aarti A Chavan
 //  Date :              16/04/2024
 //
 ////////////////////////////////////////////////////////////
